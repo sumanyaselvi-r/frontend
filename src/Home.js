08b2b3d9ce73b6b0307408def28b1acc0d85f293
sumanyaselvi-r <145419732+sumanyaselvi-r@users.noreply.github.com>
2024-01-02 14:44:41 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,7 @@ import About from './About';
 import Footer from './Footer';
 import Logo from "./images/Test.png"
 import Hcontact from './Hcontact';
+import bgVideo from './video/wheat.mp4';
 
 function Home() {
   return (
@@ -18,7 +19,9 @@ function Home() {
         <h5 style={{fontWeight:"bolder"}}>Galaxy Bio Tech</h5>
         <p>"Fertilizing Tomorrow's Harvests Today."</p><br/><br/><br/><br/><br/><br/>
      
-       
+        <video className="back-video"  loop muted playsInline autoPlay >
+          <source src={bgVideo} type="video/mp4"/>
+        </video>
 
          </div></section>
          <About/>
